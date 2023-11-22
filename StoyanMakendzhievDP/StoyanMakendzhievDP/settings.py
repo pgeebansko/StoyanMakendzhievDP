a2b0040b3d2ea76b29b89fc3fd2b1d2cf97308a3
Isme04 <ismahodja7@gmail.com>
2023-11-22 15:36:12 +0200

    22.11.2023

--- StoyanMakendzhievDP/StoyanMakendzhievDP/settings.py
+++ StoyanMakendzhievDP/StoyanMakendzhievDP/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,69 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from polls.models import Choice, Question
+Question.objects.all()
+from django.utils import timezone
+q = Question(question_text="What's new?", pub_date=timezone.now())
+q.save()
+q.id
+q.question_text
+q.pub_date
+q.question_text = "What's up?"
+q.save()
+Question.objects.all()
+
+
+Question.objects.all()
+
+
+Question.objects.filter(id=1)
+
+Question.objects.filter(question_text__startswith="What")
+
+from django.utils import timezone
+current_year = timezone.now().year
+Question.objects.get(pub_date__year=current_year)
+
+
+Question.objects.get(id=2)
+
+
+Question.objects.get(pk=1)
+
+
+q = Question.objects.get(pk=1)
+q.was_published_recently()
+
+
+q = Question.objects.get(pk=1)
+
+q.choice_set.all()
+
+
+q.choice_set.create(choice_text="Not much", votes=0)
+
+q.choice_set.create(choice_text="The sky", votes=0)
+
+c = q.choice_set.create(choice_text="Just hacking again", votes=0)
+
+
+c.question
+
+
+q.choice_set.all()
+
+q.choice_set.count()
+
+
+Choice.objects.filter(question__pub_date__year=current_year)
+
+c = q.choice_set.filter(choice_text__startswith="Just hacking")
+c.delete()
+
+
+
+
+
